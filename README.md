426017a041d4497965650674bd6e93ebd18443c5
Justinas Ci <justinas.ciucelis@gmail.com>
2019-08-15 22:32:04 +0300

    Added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Job-listing APP
+
+Special thanks to Brad for this tutorial
+You can find it at https://www.youtube.com/watch?v=LEkjrQMmIK0
--- notes.php
+++
@@ -1,5 +0,0 @@
-<?php 
-/**
- * Special thanks to Brad for this tutorial
- * You can find it at https://www.youtube.com/watch?v=LEkjrQMmIK0
- */
